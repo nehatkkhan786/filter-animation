ebce712d570546c2241bc78ff9b6e68a6c3be060
ABDUL RASID <abdulrasid@ABDULs-MacBook-Pro.local>
2022-10-09 22:43:31 +0530

    added genre buttons

--- src/components/FIlterButton.jsx
+++ src/components/FIlterButton.jsx
@@ -10,16 +10,18 @@ const FIlterButton = ({genres, setFilterMovies, popularmovies}) => {
 
    const clickHandle =(id)=>{
     setActive(id);
+    console.log(id)
    }
     
   return (
     <div className='genre-button-wrapper'>
-       <button onClick={()=>setActive(allId)} className={active == allId ? 'btn actives' : 'btn'}>All</button>
+       <button onClick={()=>setActive(allId)} className={active == allId ? 'button-17 button-17-active button-17-focus' : 'button-17'}>All</button>
         {genres?.map((item)=>(
-            <button onClick={()=>clickHandle(item.id)} key={item.id} className={active == item.id ? 'btn actives' : 'btn'}>{item.name}</button>
+            <button onClick={()=>clickHandle(item.id)} key={item.id} className={active == item.id ? 'button-17 button-17-active button-17-focus' : 'button-17'}>{item.name}</button>
         ))}
     </div>
   )
 }
 
 export default FIlterButton
+
